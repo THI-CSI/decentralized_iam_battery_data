2fd5c4ecf4fd6268a32f7db1d5b49b1effba420d
Jonas Ampferl <ampferl.jonas@gmail.com>
2025-04-21 12:21:50 -0400

    feat: Implement Functions to Generate Blocks (#43)

--- blockchain/cmd/main.go
+++ blockchain/cmd/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"blockchain/internal/core"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf("IAM Blockchain\n")
+	var chain []core.Block
+
+	// Generate one Genisis block and five blocks without transactions
+	chain = append(chain, core.GenerateGenisisBlock())
+	for i := 0; i <= 5; i++ {
+		chain = append(chain, core.GenerateBlock(chain[len(chain)-1], nil))
+	}
+
+	// Print the blockchain example to the console
+	for i := range chain {
+		fmt.Println(chain[i])
+	}
 }
--- blockchain/internal/core/block.go
+++ blockchain/internal/core/block.go
@@ -1,6 +1,10 @@
 package core
 
-import "time"
+import (
+	"crypto/sha3"
+	"encoding/hex"
+	"time"
+)
 
 // Block represents a unit in the blockchain containing a set of Transactions.
 // Each block is cryptographically linked to the previous one.
@@ -15,4 +19,41 @@ type Block struct {
 	PreviousBlockHash string
 	// Transactions holds all transactions in this block.
 	Transactions []Transaction
+}
+
+// Calculates the SHA-256 hash of a block
+func CalculateBlockHash(block Block) string {
+	sha256 := sha3.New256()
+	// TODO: We have to calculate a Merkle Tree Hash and add it to the record
+	record := string(block.Index) + block.Timestamp.String() + block.PreviousBlockHash
+	sha256.Write([]byte(record))
+	hash := sha256.Sum(nil)
+	return hex.EncodeToString(hash)
+}
+
+// Generate a Genisis Block
+func GenerateGenisisBlock() Block {
+	var block Block
+
+	block.Index = 0
+	block.Timestamp = time.Now()
+	// Since this is the first block in the chain, the PreviousBlockHash is hardcoded
+	block.PreviousBlockHash = "0000000000000000000000000000000000000000000000000000000000000000"
+	block.Transactions = nil
+	block.Hash = CalculateBlockHash(block)
+
+	return block
+}
+
+// Generate a Block with the data of the previous block and a list of transactions
+func GenerateBlock(currentBlock Block, transactions []Transaction) Block {
+	var block Block
+
+	block.Index = currentBlock.Index + 1
+	block.Timestamp = time.Now()
+	block.PreviousBlockHash = currentBlock.Hash
+	block.Transactions = transactions
+	block.Hash = CalculateBlockHash(block)
+
+	return block
 }
