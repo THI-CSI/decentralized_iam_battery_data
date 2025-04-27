6ed17e828f30bb2e3b0960d4f65420825cdfe5b1
M4tse <mm903-1@bingo-ev.de>
2025-04-27 13:09:34 +0200

    feat: Can send dummy data and Pings over the Ethernet

--- bms/ra_gen/net_thread.c
+++ bms/ra_gen/net_thread.c
@@ -2,67 +2,72 @@
 #include "net_thread.h"
 
 #if 1
-static StaticTask_t net_thread_memory;
-#if defined(__ARMCC_VERSION)           /* AC6 compiler */
+                static StaticTask_t net_thread_memory;
+                #if defined(__ARMCC_VERSION)           /* AC6 compiler */
                 static uint8_t net_thread_stack[4096] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.thread") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
                 #else
-static uint8_t net_thread_stack[4096] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.net_thread") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
-#endif
-#endif
-TaskHandle_t net_thread;
-void net_thread_create(void);
-static void net_thread_func(void *pvParameters);
-void rtos_startup_err_callback(void *p_instance, void *p_data);
-void rtos_startup_common_init(void);
+                static uint8_t net_thread_stack[4096] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.net_thread") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
+                #endif
+                #endif
+                TaskHandle_t net_thread;
+                void net_thread_create(void);
+                static void net_thread_func(void * pvParameters);
+                void rtos_startup_err_callback(void * p_instance, void * p_data);
+                void rtos_startup_common_init(void);
 extern uint32_t g_fsp_common_thread_count;
 
-const rm_freertos_port_parameters_t net_thread_parameters =
-{ .p_context = (void*) NULL, };
+                const rm_freertos_port_parameters_t net_thread_parameters =
+                {
+                    .p_context = (void *) NULL,
+                };
 
-void net_thread_create(void)
-{
-    /* Increment count so we will know the number of threads created in the RA Configuration editor. */
-    g_fsp_common_thread_count++;
+                void net_thread_create (void)
+                {
+                    /* Increment count so we will know the number of threads created in the RA Configuration editor. */
+                    g_fsp_common_thread_count++;
 
-    /* Initialize each kernel object. */
+                    /* Initialize each kernel object. */
+                    
 
-#if 1
-    net_thread = xTaskCreateStatic (
-#else
+                    #if 1
+                    net_thread = xTaskCreateStatic(
+                    #else
                     BaseType_t net_thread_create_err = xTaskCreate(
                     #endif
-                                    net_thread_func,
-                                    (const char*) "Net Thread", 4096 / 4, // In words, not bytes
-                                    (void*) &net_thread_parameters, //pvParameters
-                                    2,
-#if 1
-                                    (StackType_t*) &net_thread_stack,
-                                    (StaticTask_t*) &net_thread_memory
-#else
+                        net_thread_func,
+                        (const char *)"Net Thread",
+                        4096/4, // In words, not bytes
+                        (void *) &net_thread_parameters, //pvParameters
+                        2,
+                        #if 1
+                        (StackType_t *)&net_thread_stack,
+                        (StaticTask_t *)&net_thread_memory
+                        #else
                         & net_thread
                         #endif
-                                    );
+                    );
 
-#if 1
-    if (NULL == net_thread)
-    {
-        rtos_startup_err_callback (net_thread, 0);
-    }
-#else
+                    #if 1
+                    if (NULL == net_thread)
+                    {
+                        rtos_startup_err_callback(net_thread, 0);
+                    }
+                    #else
                     if (pdPASS != net_thread_create_err)
                     {
                         rtos_startup_err_callback(net_thread, 0);
                     }
                     #endif
-}
-static void net_thread_func(void *pvParameters)
-{
-    /* Initialize common components */
-    rtos_startup_common_init ();
+                }
+                static void net_thread_func (void * pvParameters)
+                {
+                    /* Initialize common components */
+                    rtos_startup_common_init();
 
-    /* Initialize each module instance. */
+                    /* Initialize each module instance. */
+                    
 
-#if (1 == BSP_TZ_NONSECURE_BUILD) && (1 == 1)
+                    #if (1 == BSP_TZ_NONSECURE_BUILD) && (1 == 1)
                     /* When FreeRTOS is used in a non-secure TrustZone application, portALLOCATE_SECURE_CONTEXT must be called prior
                      * to calling any non-secure callable function in a thread. The parameter is unused in the FSP implementation.
                      * If no slots are available then configASSERT() will be called from vPortSVCHandler_C(). If this occurs, the
@@ -74,6 +79,6 @@ static void net_thread_func(void *pvParameters)
                      portALLOCATE_SECURE_CONTEXT(0);
                     #endif
 
-    /* Enter user code for this thread. Pass task handle. */
-    net_thread_entry (pvParameters);
-}
+                    /* Enter user code for this thread. Pass task handle. */
+                    net_thread_entry(pvParameters);
+                }
--- bms/ra_gen/net_thread.h
+++ bms/ra_gen/net_thread.h
@@ -2,15 +2,15 @@
 #ifndef NET_THREAD_H_
 #define NET_THREAD_H_
 #include "bsp_api.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "semphr.h"
-#include "hal_data.h"
-#ifdef __cplusplus
+                #include "FreeRTOS.h"
+                #include "task.h"
+                #include "semphr.h"
+                #include "hal_data.h"
+                #ifdef __cplusplus
                 extern "C" void net_thread_entry(void * pvParameters);
                 #else
-extern void net_thread_entry(void *pvParameters);
-#endif
+                extern void net_thread_entry(void * pvParameters);
+                #endif
 FSP_HEADER
 FSP_FOOTER
 #endif /* NET_THREAD_H_ */
