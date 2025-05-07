32975440159b4bf632faa5aad2f369aacf308249
valljah <haerter.valentin@gmail.com>
2025-05-07 12:17:14 +0200

    feat: Switch database to encrypted TinyDB
    Pair-programmed by @devbod @valljah

--- cloud/util/middleware.py
+++ cloud/util/middleware.py
@@ -1,37 +1,20 @@
-from functools import wraps
 import json
 from fastapi import HTTPException
-from mycrypto.mycrypto import decrypt_and_verify, load_private_key
-from util.models import ItemCreatePlain #ItemCreate
 
-def decrypt_request_data(func):
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        request_data = kwargs.get('item')
-        if not request_data or not hasattr(request_data, 'encrypted_data'):
-            raise HTTPException(
-                status_code=400,
-                detail="Verschlüsselte Daten fehlen"
-            )
-            
-        try:
-            receiver_key = load_private_key("IHR_PASSWORT")
-            sender_key = None
-            
-            decrypted_data = decrypt_and_verify(
-                receiver_key,
-                sender_key,
-                request_data.encrypted_data.dict()
-            )
-            
-            decoded_data = json.loads(decrypted_data.decode())
-            kwargs['item'] = ItemCreate(**decoded_data)
-            
-        except ValueError as e:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Entschlüsselungsfehler: {str(e)}"
-            )
-            
-        return await func(*args, **kwargs)
-    return wrapper
+from crypto.crypto import decrypt_and_verify
+from Crypto.PublicKey import ECC
+from util.models import EncryptedPayload
+
+
+def verify_request(item: EncryptedPayload, private_key: ECC.EccKey) -> dict:
+    try:
+        decrypted_bytes = decrypt_and_verify(
+            private_key,
+            item.model_dump()
+        )
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Failed decryption or signature verification.")
+    try:
+        return json.loads(decrypted_bytes.decode("utf-8"))
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400, detail="Failed decoding decrypted data to JSON.")
--- cloud/util/models.py
+++ cloud/util/models.py
@@ -1,24 +1,7 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel
 
-Base = declarative_base()
 
-# SQLAlchemy Modell
-class Item(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    description = Column(String)
-
-# Pydantic Modelle
-#class EncryptedPayload(BaseModel):
-#    enc: str
-#    ciphertext: str
-
-#class ItemCreate(BaseModel):
-#    encrypted_data: EncryptedPayload
-
-class ItemCreatePlain(BaseModel):
-    name: str
-    description: str
+# Pydantic models
+class EncryptedPayload(BaseModel):
+    enc: str
+    ciphertext: str
