b6db716d0c48f59b0d0e601baacdd84e8e6b5851
Falo04 <felixwallner0@gmail.com>
2025-05-17 20:49:41 +0200

    fix: overflow-hidden for application, fixed scrolling

--- blockchain/frontend/src/components/BaseLayout.tsx
+++ blockchain/frontend/src/components/BaseLayout.tsx
@@ -10,7 +10,7 @@ export function BaseLayout({
     navbar: React.ReactNode;
 }>) {
     return (
-        <div className="relative isolate flex h-svh w-full flex-col bg-white lg:overflow-hidden lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
+        <div className="relative isolate flex h-svh w-full flex-col bg-white lg:overflow-x-hidden lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
             <div>{navbar}</div>
 
             <div className="dark:lg:rind-white/10 m-2 flex grow p-3 md:p-8 lg:rounded-lg lg:bg-white lg:shadow-xs lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900">
