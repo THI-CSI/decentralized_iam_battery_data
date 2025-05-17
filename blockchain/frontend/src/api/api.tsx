4fe44c314f7aea02809e0a4d00b7f552c8b846cd
Falo04 <felixwallner0@gmail.com>
2025-05-17 20:43:30 +0200

    feat: generated API for frontend

---
+++ blockchain/frontend/src/api/api.tsx
@@ -0,0 +1,36 @@
+import {
+    type ApiResponse,
+    type ApiV1BlocksBlockIdGetRequest,
+    type ApiV1BlocksBlockIdTransactionsGetRequest,
+    type ApiV1DidsDidGetRequest,
+    type ApiV1VcUrnGetRequest,
+    BlocksApi,
+    Configuration,
+    type CoreDid,
+    DIDsApi,
+    VCsApi,
+} from "@/api/generated";
+
+const configuration = new Configuration({
+    basePath: window.location.origin,
+});
+
+const didApi = new DIDsApi(configuration);
+const vcApi = new VCsApi(configuration);
+const blockApi = new BlocksApi(configuration);
+
+export const Api = {
+    did: {
+        getAll: () => didApi.apiV1DidsGet(),
+        getSingle: (request: ApiV1DidsDidGetRequest) => didApi.apiV1DidsDidGet(request),
+    },
+    vc: {
+        get: (request: ApiV1VcUrnGetRequest) => vcApi.apiV1VcUrnGet(request),
+    },
+    block: {
+        getAll: () => blockApi.apiV1BlocksGet(),
+        getSingle: (request: ApiV1BlocksBlockIdGetRequest) => blockApi.apiV1BlocksBlockIdGet(request),
+        getTransaction: (request: ApiV1BlocksBlockIdTransactionsGetRequest) =>
+            blockApi.apiV1BlocksBlockIdTransactionsGet(request),
+    },
+};
