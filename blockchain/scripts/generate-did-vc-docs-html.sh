a2cfbc2929f0cb29f47370b38db6c4c75b272fb6
Fynnwind <64282552+Fynnwind@users.noreply.github.com>
2025-05-21 01:39:43 +0200

    wip: fix schemas & add html docs

---
+++ blockchain/scripts/generate-did-vc-docs-html.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Generating DID and VC docs ..."
+
+SCHEMA_DIR="./internal/jsonschema"
+DOCS="./docs/schema/html"
+VENV=".venv"
+
+mkdir -p "$DOCS"
+
+for file in "$SCHEMA_DIR"/*.json; do
+  echo "Generating html docs for: $file"
+  "$VENV/bin/generate-schema-doc" "$file"
+  mv schema_doc.html "$DOCS/$(basename "$file" .json).html"
+done
