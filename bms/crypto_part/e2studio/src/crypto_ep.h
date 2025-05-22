fdccd7a18f69bcb6b3979df96ab22b170457dea7
flosmue <flm2397@thi.de>
2025-05-23 00:13:42 +0200

    feat: Added basic crypto functions

---
+++ bms/crypto_part/e2studio/src/crypto_ep.h
@@ -0,0 +1,35 @@
+/***********************************************************************************************************************
+ * File Name    : crypto_ep.h
+ * Description  : Contains data structures and functions used in crypto_ep.h.
+ **********************************************************************************************************************/
+
+/***********************************************************************************************************************
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+***********************************************************************************************************************/
+
+#ifndef CRYPTO_EP_H_
+#define CRYPTO_EP_H_
+
+/* Macro definitions.*/
+#define ECC_256_BIT_LENGTH        (256U)              /* key size in bits for ECC*/
+
+#define ECC_256_EXPORTED_SIZE     (500U)              /* Size of the data buffer(to store key data) in bytes for ECC*/
+#define AES_256_EXPORTED_SIZE     (500U)              /* Size of the data buffer(to store key data) in bytes for AES*/
+#define TAG_LENGTH                ((size_t)16U)       /* number of bytes that make up the returned tag.*/
+#define AES_KEY_ID                ((psa_key_id_t) 5)  /* persistent identifier for the key.*/
+#define ECC_KEY_ID                ((psa_key_id_t) 6)
+#define AES_KEY_BITS              (256U)             /* key size in bits for AES*/
+#define ECC_RSA_PAYLOAD_SIZE      (30U)
+
+/*Function declaration */
+psa_status_t aes_operation(void);
+void handle_error(psa_status_t status, char * err_str);
+psa_status_t sha_operation(void);
+psa_status_t ecc_operation(void);
+fsp_err_t littlefs_init(void);
+void deinit_littlefs(void);
+psa_status_t ecc_rsa_hashing_operation(unsigned char * payload, uint8_t * payload_hash, size_t * payload_hash_len);
+
+#endif /* CRYPTO_EP_H_ */
