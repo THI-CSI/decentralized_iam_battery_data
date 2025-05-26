4171d202c890e7ce944e66452f23da10a6e099d7
flosmue <flm2397@thi.de>
2025-05-26 21:00:02 +0200

    feat: Added base64 encoding

--- mock_bms/src/crypto.py
+++ mock_bms/src/crypto.py
@@ -14,6 +14,11 @@ did = "did:example:123456789abcdefgh"
 # Generate signing key pair
 private_signing_key = ec.generate_private_key(ec.SECP256R1())
 public_signing_key = private_signing_key.public_key()
+siging_public_key_der = public_signing_key.public_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+print("[*] Signing public key: ", siging_public_key_der)
 
 # Genertate Ephermal key pair for key agreement
 private_ephermal_key = ec.generate_private_key(ec.SECP256R1())
@@ -22,6 +27,12 @@ public_ephermal_key = private_ephermal_key.public_key()
 # Generate Key Pair for Cloud
 private_cloud_key = ec.generate_private_key(ec.SECP256R1())
 public_cloud_key = private_cloud_key.public_key()
+cloud_private_key_der = private_cloud_key.private_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+print("[*] Cloud private key: ", cloud_private_key_der)
 
 # Define dynamic battery data
 dynamic_battery_data = {
@@ -58,15 +69,28 @@ nonce = os.urandom(12)
 aesgcm = AESGCM(aes_derived_key)
 ciphertext = aesgcm.encrypt(nonce, dynamic_battery_data_bytes, nonce)
 
-# Create message 
-message = f'"ciphertext": {ciphertext}, "aad": {nonce}, "salt": {salt}, "ephermal_public_key": {ephermal_public_key_der}, "did": {did}'
-message_bytes = message.encode("utf-8")
+# Create message
+ciphertext_b64 = base64.b64encode(ciphertext).decode('utf-8')
+nonce_b64 = base64.b64encode(nonce).decode('utf-8')
+salt_b64 = base64.b64encode(salt).decode('utf-8')
+ephermal_public_key_b64 = base64.b64encode(ephermal_public_key_der).decode('utf-8')
+message = {
+    "ciphertext": ciphertext_b64,
+    "aad": nonce_b64,
+    "salt": salt_b64,
+    "ephermal_public_key": ephermal_public_key_b64,
+    "did": did
+}
+message_json = json.dumps(message)
+message_bytes = message_json.encode("utf-8")
 
 # Sign messsage with ECDSA and add to message
 signature = private_signing_key.sign(
     message_bytes,
     ec.ECDSA(hashes.SHA256())
 )
-message = f'"ciphertext": {ciphertext}, "aad": {nonce}, "salt": {salt}, "ephermal_public_key": {ephermal_public_key_der}, "did": {did}, "signature": {signature}'
-message_bytes = message.encode("utf-8")
-print(message_bytes)
+signature_b64 = base64.b64encode(signature).decode('utf-8')
+message["signature"] = signature_b64
+signed_json_message = json.dumps(message)
+signed_json_message_bytes = signed_json_message.encode("utf-8")
+print("[*] Signed json message (bytes): ", signed_json_message_bytes)
