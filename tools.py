1d2ae3210f83322932c01294bb86ff8143adbb4d
Jonas Ampferl <ampferl.jonas@gmail.com>
2025-06-18 15:29:34 +0200

    fix: Add check to create blockchain.json if it does not exist

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import subprocess
 import signal
+import os
 
 DOCKER_COMMANDS = [
     "up",
@@ -26,6 +27,19 @@ def check_return_code(code):
         print(f"Command failed with code {code}", file=sys.stderr)
         sys.exit(code)
 
+def check_return_code(code):
+    if code != 0:
+        print(f"Command failed with code {code}", file=sys.stderr)
+        sys.exit(code)
+
+
+def blockchain_cmd(unknown_args):
+    cwd = os.getcwd()
+    os.chdir('blockchain')
+    process = subprocess.run(["go", "run", "./cmd/main.go", *unknown_args])
+    os.chdir(cwd)
+    check_return_code(process.returncode)
+
 
 def docker_compose(command: str, unknown_args):
     process = subprocess.run(["docker", "compose", command, *unknown_args])
@@ -35,6 +49,9 @@ def main():
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers(dest="command")
     subparser.required = True
+
+    if not os.path.exists("./blockchain/blockchain.json"):
+        blockchain_cmd(["-genesis"])
 
     for c in DOCKER_COMMANDS:
         subparser.add_parser(c)
