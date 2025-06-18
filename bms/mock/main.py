485a8752b6c347ac36295c58eb67836c26624b01
Jonas Ampferl <ampferl.jonas@gmail.com>
2025-06-18 20:36:36 +0200

    feat: Implement a Sign Service for the Client

--- bms/mock/main.py
+++ bms/mock/main.py
@@ -5,42 +5,69 @@ from utils.data_gen import run_battery_data_generator
 from utils.message_creation import message_creation
 
 BLOCKCHAIN_URL = "http://localhost:8443"
+SIGN_SERVICE_URL = "http://localhost:8123"
+
+# Environment Variables
+# - CONTROLLER_DID
+# - VERIFICATION_METHOD
+
+if __name__ == "__main__":
+    # TODO 1. Generate BMS Key Pair
+    controller_did = os.getenv("CONTROLLER_DID", "did:batterypass:oem.audi")
+
+    # TODO 2. Generate BMS DID
+    did_bms = "did:batterypass:bms.abcd"
+
+    did_document = {}
+    # 3. Sign DID with Client's sign service
+    response = requests.post(f"{SIGN_SERVICE_URL}/sign/", json={
+        "did": did_bms,
+        "verification_method": os.getenv("VERIFICATION_METHOD", "")
+    })
+    if response.status_code != 200:
+        print("Error while signing DID.")
+        exit(1)
+
+    signed_did = response.json()
+
+    # 4. Register BMS on Blockchain
+    response = requests.post(f"{BLOCKCHAIN_URL}/api/v1/dids/createormodify", json=signed_did)
+
+    if response.status_code != 200:
+        print("Error while registering BMS on Blockchain.")
+        exit(1)
+    print("BMS registered successfully.")
+
+    # TODO 5. Get DID from Controller (OEM)
+
+    # TODO 6. Create VC
+
+    # TODO 7. Sign VC
+
+    # TODO 8. Register VC.
 
 
-# TODO 1. Key Generierenen
-os.getenv("CONTROLLER_DID", "did:batterypass:oem.audi")
 
-# TODO 2. DID generieren
-did_bms = "did:batterypass:bms.abcd"
-
-did_document = {}
-# TODO 3. Client zum signieren des DIDs nutzen
-## Client Endpoint to sign a DID
-
-# TODO 4. Register BMS on Blockchain
-requests.post(f"{BLOCKCHAIN_URL}/api/v1/dids/createormodify", json={"id": did_bms, "service": [{"type": "BMS", "serviceEndpoint": "http://localhost:8080/bms"}]})
-print("BMS registered successfully.")
-
-# TODO 5. Get DID from Controller (OEM)
+    vcs = ["did:batterypass:cloud.sn-central"]
+    dids = []
+    for vc in vcs:
+        dids.append(requests.get(f"{BLOCKCHAIN_URL}/api/v1/dids/{vc}").json())
 
 
-vcs = ["did:batterypass:cloud.sn-central"]
-dids = []
-for vc in vcs:
-    dids.append(requests.get(f"{BLOCKCHAIN_URL}/api/v1/dids/{vc}").json())
+    # Generate Data
+    battery_data = run_battery_data_generator()
 
+    for did in dids:
+        url = did["service"][0]["serviceEndpoint"]
+        did_string = did["id"]
+        public_key = did["verificationMethod"]["publicKeyMultibase"]
 
-# 6. Daten generieren
-battery_data = run_battery_data_generator()
+        # TODO Encrypt Data for Cloud
+        #encrypted_data = message_creation(battery_data, public_key)
+        encrypted_data = battery_data
 
-for did in dids:
-    url = did["service"][0]["serviceEndpoint"]
-    did_string = did["id"]
-    public_key = did["verificationMethod"]["publicKeyMultibase"]
-    # TODO 7. Encrypt Data for Cloud
-    #encrypted_data = message_creation(battery_data, public_key)
-    encrypted_data = battery_data
-    # 8. Upload Data
-    response = requests.post(f"{url}/batterypass/{did_bms}", json={"data": encrypted_data})
-    if response.status_code == 200:
-        print(f"Data for {did_string} sent successfully.")
+        # Upload Data
+        response = requests.post(f"{url}/batterypass/{did_bms}", json={"data": encrypted_data})
+        if response.status_code == 200:
+            print(f"Data for {did_string} sent successfully.")
+
---
+++ client/sign_service.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Body, Response
+from typing import Any
+from crypto.crypto import sign_did_external
+
+import sys
+import json
+
+app = FastAPI()
+
+@app.post("/sign/")
+def sign_service(response: Response, request: Any = Body(None)):
+    response.status_code = 400
+
+    try:
+        data = json.loads(request)
+    except:
+        return {"error": "Invalid JSON"}
+
+    if "did" not in data:
+        return {"error": "did is required"}
+    if "verification_method" not in data:
+        return {"error": "verification_method is required"}
+    did = data["did"]
+    verification_method = data["verification_method"]
+
+    try:
+        signed_did = sign_did_external(did, verification_method)
+    except:
+        return {"error": "Failed to sign DID"}
+
+    response.status_code = 200
+    return signed_did
+
+# Run it with:
+# ```shell
+# uvicorn sign_service:app 8123
+# ```
